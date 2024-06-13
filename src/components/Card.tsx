09399aab67634e338073fe9c9756e624610579ce
kamusari <ovsyannikovkostyan@gmail.com>
2024-06-13 20:38:14 +0400

    change card layout

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -33,24 +33,26 @@ const Card = ({ id }: CardProps) => {
 				<Link to={`/manga/${manga.id}`}>
 					<img className={styles.card__cover} src={manga.wrap_path} alt="manga-cover" />
 				</Link>
-				<p className={styles.card__title}>{manga.title}</p>
-				<div className={styles.card__bottomLine}>
-					<div className={styles.card__bottomLine__left}>
-						<span>Цена:</span>
-						<p>{manga.price} руб.</p>
-					</div>
-					<div className={styles.card__bottomLine__right}>
-						<img className={styles.card_bottomLine__right_button} onClick={onClickFavorite} src={manga.isFavorite ? '/icons/liked.svg' : '/icons/unliked.svg'} alt="like-btn" />
-						<div className={styles.card__bottomLine__right_addButtons}>
-							{manga.inCart > 0 ? (
-								<>
+				<div className={styles.cart__info}>
+					<p className={styles.card__title}>{manga.title}</p>
+					<div className={styles.card__bottomLine}>
+						<div className={styles.card__bottomLine__left}>
+							<p>Цена:</p>
+							<p>{manga.price} руб.</p>
+						</div>
+						<div className={styles.card__bottomLine__right}>
+							<img className={styles.card_bottomLine__right_button} onClick={onClickFavorite} src={manga.isFavorite ? '/icons/liked.svg' : '/icons/unliked.svg'} alt="like-btn" />
+							<div className={styles.card__bottomLine__right_addButtons}>
+								{manga.inCart > 0 ? (
+									<>
+										<img className={styles.card_bottomLine__right_button} onClick={onClickPlus} src={manga.inCart > 0 ? '/icons/plus.svg' : '/icons/not-added.svg'} alt="plus-btn" />
+										<img className={styles.card_bottomLine__right_button} onClick={onClickMinus} src="/icons/minus.svg" alt="minus-btn" />
+										<b className={styles.card__bottomLine__right_button_count}>{manga.inCart}</b>
+									</>
+								) : (
 									<img className={styles.card_bottomLine__right_button} onClick={onClickPlus} src={manga.inCart > 0 ? '/icons/plus.svg' : '/icons/not-added.svg'} alt="plus-btn" />
-									<img className={styles.card_bottomLine__right_button} onClick={onClickMinus} src="/icons/minus.svg" alt="minus-btn" />
-									<b className={styles.card__bottomLine__right_button_count}>{manga.inCart}</b>
-								</>
-							) : (
-								<img className={styles.card_bottomLine__right_button} onClick={onClickPlus} src={manga.inCart > 0 ? '/icons/plus.svg' : '/icons/not-added.svg'} alt="plus-btn" />
-							)}
+								)}
+							</div>
 						</div>
 					</div>
 				</div>
