9ede27dd715352893f330b87ae723a7da6475ae2
kamusari <ovsyannikovkostyan@gmail.com>
2024-06-14 15:23:07 +0400

    make not fount section component

--- src/pages/Favorite.tsx
+++ src/pages/Favorite.tsx
@@ -1,8 +1,8 @@
 import { useEffect } from 'react'
-import { CiFaceMeh } from 'react-icons/ci'
-import { Link } from 'react-router-dom'
 import { Card } from '../components/Card'
 import { Header } from '../components/Header'
+import NotFoundSection from '../components/UI/NotFoundSection'
+import { PokerFace } from '../components/UI/icons/Faces'
 import styles from '../scss/pages/Favorite.module.sass'
 import { useManga } from '../store'
 
@@ -19,21 +19,7 @@ const Favorite = () => {
 			<Header />
 			<div className={styles.section}>
 				<p className={styles.section__title}>Мои закладки</p>
-				<div className={styles.section__list}>
-					{favorites.length ? (
-						favorites.map(manga => <Card key={manga.id} {...manga} />)
-					) : (
-						<div className={styles.section__list__notFound}>
-							<CiFaceMeh size={200} className={styles.section__list__notFound__icon} />
-							<b>Закладок нет</b>
-							<span>Вы ничего не добавляли в них ...</span>
-							<Link to={'/'} className={styles.section__list__notFound__button}>
-								<img src="/icons/arrow-white.svg" alt="arrow" />
-								<p>Вернуться</p>
-							</Link>
-						</div>
-					)}
-				</div>
+				<div className={styles.section__list}>{favorites.length ? favorites.map(manga => <Card key={manga.id} {...manga} />) : <NotFoundSection message1="Закладок нет" message2="Вы ничего не добавляли в них ..." face={<PokerFace />} />}</div>
 			</div>
 		</>
 	)
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,7 +1,7 @@
-import { CiFaceFrown } from 'react-icons/ci'
-import { Link } from 'react-router-dom'
 import { Card } from '../components/Card'
 import { Header } from '../components/Header'
+import { SadFace } from '../components/UI/icons/Faces'
+import NotFoundSection from '../components/UI/NotFoundSection'
 import styles from '../scss/pages/Profile.module.sass'
 import { useManga } from '../store'
 
@@ -14,22 +14,7 @@ const Profile = () => {
 			<Header />
 			<div className={styles.section}>
 				<div className={styles.section__title}>Мои покупки</div>
-				<div className={styles.section__list}>
-					{purchasesAmount > 0 ? (
-						purchases.map(manga => <Card key={manga.id} {...manga}></Card>)
-					) : (
-						<div className={styles.section__list__notFound}>
-							<CiFaceFrown size={200} className={styles.section__list__notFound__icon} />
-							<b>У вас нет заказов</b>
-							<span>Вы нищеброд?</span>
-							<span>Оформите хотя бы один заказ.</span>
-							<Link to={'/'} className={styles.section__list__notFound__button}>
-								<img src="/icons/arrow-white.svg" alt="arrow" />
-								<p>Вернуться</p>
-							</Link>
-						</div>
-					)}
-				</div>
+				<div className={styles.section__list}>{purchasesAmount > 0 ? purchases.map(manga => <Card key={manga.id} {...manga}></Card>) : <NotFoundSection message1="У вас нет заказов" message2="Оформите хотя бы один заказ..." face={<SadFace />} />}</div>
 			</div>
 		</>
 	)
